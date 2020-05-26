82679bc85d07ba151605ab1bb8f5b0019c2fcffe
adjo <calvin@kairuconsulting.com>
2020-05-25 21:23:45 -0400

    report updated to show all the photos from the appraisal

--- DB/AppraisalsContext.cs
+++ DB/AppraisalsContext.cs
@@ -136,7 +136,7 @@ namespace testAAP.DB
                     }
 
                     // sources 
-                    list.source.vehicle_sources_comment = MySQLDBExtension.GetStringSafe(reader, "vehicle_sources_comment");
+                    list.source.vehicle_sources_comment = System.Net.WebUtility.UrlDecode(MySQLDBExtension.GetStringSafe(reader, "vehicle_sources_comment"));
                     list.source.vehicle_sources_haggerty = MySQLDBExtension.GetStringSafe(reader, "vehicle_sources_haggerty");
                     list.source.vehicle_sources_kbb = MySQLDBExtension.GetStringSafe(reader, "vehicle_sources_kbb");
                     list.source.vehicle_sources_nada = MySQLDBExtension.GetStringSafe(reader, "vehicle_sources_nada");
